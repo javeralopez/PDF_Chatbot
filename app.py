38846ca2dc4a75c2ed5c4950b8ae8465fc8b964a
Pratham Yeshwante <148319246+Pratham0405@users.noreply.github.com>
2024-04-22 22:09:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def get_conversational_chain():
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     
-    new_db = FAISS.load_local("faiss_index", embeddings)
+    new_db = FAISS.load_local("faiss_index", embeddings, cfg.DB_FAISS_PATH)
     docs = new_db.similarity_search(user_question)
 
     chain = get_conversational_chain()
